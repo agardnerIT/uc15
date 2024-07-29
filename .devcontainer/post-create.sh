6bdb36bc254ab11cdaadfb3e735951b1f6cb3867
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-07-29 07:48:48 +0000

    update

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,3 +7,10 @@ pwd
 
 pip install -r /workspaces/uc15/requirements.txt
 ansible-galaxy collection install ansible.eda
+
+# Set ansible port 5000 to public
+# WARNING: This is potentially dangerous as anyone with the codespace URL + port
+# Can send events to Ansible
+# TODO: Discuss whether we should user to do this
+# manually with docs so they understand the risks posed.
+#gh codespace ports visibility 5000:public -c $CODESPACE_NAME
