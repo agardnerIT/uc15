d1bbbfc4f89ed6b40adbc88b67602c094f27f10d
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-07-29 05:09:51 +0000

    add sudo

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt update
-apt install -y openjdk-21-jdk
+sudo apt update
+sudo apt install -y openjdk-21-jdk
 
 pip install -r /$CODESPACE_VSCODE_FOLDER/requirements.txt
 ansible-galaxy collection install ansible.eda
